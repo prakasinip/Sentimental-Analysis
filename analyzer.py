4c9ea4c4071f91d899d7cf105395ff27ac72e4f3
PrakasiniP <116899856+prakasinip@users.noreply.github.com>
2024-07-03 17:16:55 +0530

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,17 +1,11 @@
 !pip install nltk
-
 !pip install numpy
-
 !pip install pandas
-
 !pip install matplotlib
 
 import nltk
-
 nltk.download('punkt')
-
 nltk.download('wordnet')
-
 nltk.download('averaged_perceptron_tagger')
 
 nltk.download('vader_lexicon')
