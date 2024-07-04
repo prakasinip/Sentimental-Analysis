303bca7afc3d2bec9e8ae913366a732176f72342
PrakasiniP <116899856+prakasinip@users.noreply.github.com>
2024-07-04 18:32:39 +0530

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -9,3 +9,22 @@ nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('vader_lexicon')
 nltk.download('punkt')
+
+import time
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import nltk
+import io
+import unicodedata
+import numpy as np
+import re
+import string
+from numpy import linalg
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import PunktSentenceTokenizer
+from nltk.tokenize import PunktSentenceTokenizer
+from nltk.corpus import webtext
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
