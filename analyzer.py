9cf67feecf653d0b8c0ce98fb732881083d31dfa
PrakasiniP <116899856+prakasinip@users.noreply.github.com>
2024-07-09 20:07:16 +0530

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -10,7 +10,6 @@ nltk.download('averaged_perceptron_tagger')
 nltk.download('vader_lexicon')
 nltk.download('punkt')
 
-import time
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -29,4 +28,12 @@ from nltk.corpus import webtext
 from nltk.stem.porter import PorterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 
+with open('kindle.txt', encoding ='ISO-8859-2') as f:
+	text = f.read()
+
+sent_tokenizer = PunktSentenceTokenizer(text)
+sents = sent_tokenizer.tokenize(text)
+
+print(word_tokenize(text))
+print(sent_tokenize(text))
 
