8db2a3a0268fa4f54149b2874bbbf9fb9c124d02
PrakasiniP <116899856+prakasinip@users.noreply.github.com>
2024-07-11 19:35:45 +0530

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -9,10 +9,17 @@ nltk.download('averaged_perceptron_tagger')
 nltk.download('vader_lexicon')
 nltk.download('punkt')
 
+import time
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import nltk
 import io
+import unicodedata
+import numpy as np
+import re
+import string
+from numpy import linalg
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.tokenize import PunktSentenceTokenizer
